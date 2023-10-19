8bf2e7bd1dd60f85ed83931d2513aeae9f1d1e14
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-10-19 12:36:11 +0200

    Add event listener to the all boxes with the grid-items class
    The event listener listens for the mouse over
    event and loops through the grid-items and adds
    a new class of blue-background to the items.
    Gave the class a background color property of
    blue in style.css

--- javascript.js
+++ javascript.js
@@ -3,15 +3,25 @@
 const gridContainer = document.querySelector("#grid-container");
 
 
+
 function makeGrid(rows, cols) {
     for (let c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
         cell.classList.add("grid-item");
         // cell.textContent = (c + 1);
 
-        console.log(cell.classList);
+        // console.log(cell.classList);
         gridContainer.appendChild(cell);
     };
 };
 
 makeGrid(16, 16);
+
+
+const gridItems = document.querySelectorAll(".grid-item");
+
+for (let i = 0; i < gridItems.length; i++) {
+    gridItems[i].addEventListener ('mouseover', (event) => {
+        event.target.classList.add("blue-background");
+    });
+}

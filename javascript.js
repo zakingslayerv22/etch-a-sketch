8aa0dd59fe02b1acafe82fdc4c4e27394afc5458
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2023-11-01 18:59:42 +0100

    Refactor the function that creates the grid.
    Change the function that creates the grid so that
    rows can be appended to columns and thus it can be
    styled with flex. Styled appropriately with flex
    in such a way that the rows and column cells
    will always shrink or grow to fit in evenly in their
    container and never change container size.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,79 @@
-//define constants
+//define
 
 const gridContainer = document.querySelector("#grid-container");
+let columns = document.querySelectorAll(".columns");
+let rows = document.querySelectorAll(".rows");
+
+
+//create rows
+function createColumns(value) {
+    for (let i = 0; i < value; i++) {
+        let column = document.createElement("div");
+        gridContainer.appendChild(column).className = "columns";
+        columns = document.querySelectorAll(".columns"); 
+    }
+}
+
+
+function createRows(rowNum) {
+    
+    for (let i = 0; i < columns.length; i++) {
+        for (let j = 0; j < rowNum; j++) {
+            let row = document.createElement("div");
+            columns[j].appendChild(row).className = "rows";
+        }
+    }
+}
 
 
 
-function makeGrid(rows, cols) {
-    for (let c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        cell.classList.add("grid-item");
-        // cell.textContent = (c + 1);
+function createGrid() {
+    createColumns(16);
+    createRows(16);
+}
 
-        // console.log(cell.classList);
-        gridContainer.appendChild(cell);
-    };
-};
+createGrid();
 
-makeGrid(16, 16);
+// window.addEventListener('load', () => {
+//     makeGrid(16)
+// });
 
 
+ 
 const gridItems = document.querySelectorAll(".grid-item");
 
-for (let i = 0; i < gridItems.length; i++) {
+function backgroundColorOnMouseOver() {
+    for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener ('mouseover', (event) => {
         event.target.classList.add("blue-background");
     });
 }
+};
+
+backgroundColorOnMouseOver()
+
+const eraserButton = document.querySelector("#eraser");
+
+function eraseGrid() {
+    eraserButton.addEventListener("click", (event) => {
+        for (let i = 0; i < gridItems.length; i++) {
+            gridItems[i].classList.remove("blue-background");
+        }
+    });
+}
+
+eraseGrid()
+
+
+
+function newUserGrid(){
+    let userPrompt = prompt("How many rows would you like?", 100);
+    if ((userPrompt > 100) || (userPrompt < 1) ) {
+        confirm("Please enter a number between 1 and 100");
+    }else {
+        makeGrid(userPrompt);
+        
+    }
+}
+
+// newUserGrid();
